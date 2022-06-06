f12bdf6fc8f4dfc3966423b1988c4ded5dc1c475
Aakash Giri <giriaakash00@gmail.com>
2022-06-06 21:15:58 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (20.50%) - LeetHub

---
+++ 100-same-tree/100-same-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean isSame = true;
+    int count = 0;
+    public void preOrder(TreeNode p, TreeNode q){
+        if(p.val != q.val || p.left == null && q.left != null || p.left != null && q.left == null || p.right == null && q.right != null || p.right != null && q.right == null){
+            // System.out.println(p.left instanceof TreeNode);
+            // System.out.println("ji "+p.val+" "+q.val+" "+p.left+" "+p.right+" "+q.left+" "+q.right);
+            this.count++;
+            if(this.count == 1){
+                this.isSame = false;   
+            }
+        }
+        if(p.left != null && q.left != null){
+            this.isSameTree(p.left, q.left);
+        }
+        if(p.right != null && q.right != null){
+            this.isSameTree(p.right, q.right);
+        }
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p != null && q != null){
+            this.preOrder(p,q);   
+        }else{
+            return false;
+        }
+        return isSame;
+    }
+}
